a8eb57a92336bd4feaf43920ddddd4c7768cc1db
guillermo <gl874391@gmail.com>
2019-10-21 18:42:59 +0000

    bundle install

--- db/migrate/20191020210105_create_quotes.rb
+++ db/migrate/20191020210105_create_quotes.rb
@@ -1,10 +1,10 @@
 class CreateQuotes < ActiveRecord::Migration[5.2]
   def change
     create_table :quotes do |t|
-    	 t.string :saying
+    	  t.string :saying
       t.string :author
-  t.timestamps
 
+      t.timestamps
     end
   end
 end
