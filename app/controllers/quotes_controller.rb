02ae626f770fd86a0d7a15b8bce454b5547c7957
guillermo <gl874391@gmail.com>
2019-10-28 18:46:44 +0000

    code change

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,6 +2,10 @@ class QuotesController < ApplicationController
 	def index
 		@quote = Quote.order("RANDOM()").first
   end
+
+  def new
+     @quote = Quote.new
+  end
   
   def create
   	 @quote = Quote.create(quote_params)
